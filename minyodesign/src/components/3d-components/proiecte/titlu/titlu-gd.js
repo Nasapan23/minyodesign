3449902b61b746b015b810d815d028087f0b914f
Nisipeanu Ionut <110778910+Nasapan23@users.noreply.github.com>
2024-10-21 21:44:51 +0300

    Diskete finished

---
+++ minyodesign/src/components/3d-components/proiecte/titlu/titlu-gd.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export function TitluGD({ position = [0, 0, 0], scale = [1, 1, 1], rotation = [0, 0, 0], ...props }) {
+  const { nodes, materials } = useGLTF('/3DObjects/titlu/titlu-gd.glb')
+
+  return (
+    <group position={position} scale={scale} rotation={rotation} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.holograma.geometry}
+        material={materials['Material.006']}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.rama_interioara.geometry}
+        material={materials['Material.005']}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.rama_interioara001.geometry}
+        material={materials['Material.007']}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.textbox.geometry}
+        material={materials['Material.004']}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload('/3DObjects/titlu/titlu-gd.glb')
--- minyodesign/src/components/portofolio/index.js
+++ minyodesign/src/components/portofolio/index.js
@@ -1,43 +1,74 @@
+"use client";
+
 import React, { useState, useMemo, useRef } from 'react';
-import { Canvas, useThree } from '@react-three/fiber';
+import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import { useSpring, animated } from '@react-spring/three';
 import { useSwipeable } from 'react-swipeable';
 import { DisketaMock } from '@/components/3d-components/diskete-portofoliu/disketa-mock';
-import { DisketaAtelier } from "@/components/3d-components/proiecte/gd/disketa-atelier.js"
+import { DisketaAtelier } from "@/components/3d-components/proiecte/gd/disketa-atelier.js";
 import { DisketaBlend } from "@/components/3d-components/proiecte/gd/disketa-blend.js";
-import { DisketaCrypto} from "@/components/3d-components/proiecte/gd/disketa-crypto.js";
-import { DisketaCulinary} from "@/components/3d-components/proiecte/gd/disketa-culinary.js";
+import { DisketaCrypto } from "@/components/3d-components/proiecte/gd/disketa-crypto.js";
+import { DisketaCulinary } from "@/components/3d-components/proiecte/gd/disketa-culinary.js";
 import { DisketaLiftUp } from "@/components/3d-components/proiecte/gd/disketa-liftup.js";
-import { PointLightHelper, DirectionalLightHelper } from 'three';
+import { DisketaOala } from "@/components/3d-components/proiecte/gd/disketa-oala.js";
+import { DisketaPhotomad } from '../3d-components/proiecte/gd/disketa-photomad';
+import { DisketaPiggy } from '../3d-components/proiecte/gd/disketa-piggy';
+import { DisketaPizza } from '../3d-components/proiecte/gd/disketa-pizza';
+import { DisketaSteam } from '../3d-components/proiecte/gd/disketa-steam';
+import { DisketaTargoviste } from '../3d-components/proiecte/gd/disketa-targoviste';
+import { DisketaUnarte } from '../3d-components/proiecte/gd/disketa-unarte';
+import { TitluGD } from '../3d-components/proiecte/titlu/titlu-gd';
+import { OrbitControls } from '@react-three/drei';
+import useIsMobile from '@/hooks/UseIsMobile';
+import * as THREE from 'three';
 
-const models = [DisketaAtelier, DisketaBlend, DisketaCrypto, DisketaCulinary, DisketaLiftUp];
+const models = [DisketaAtelier, DisketaBlend, DisketaCrypto, DisketaCulinary, DisketaLiftUp, DisketaOala, DisketaPhotomad, DisketaPiggy, DisketaPizza, DisketaSteam, DisketaTargoviste, DisketaUnarte];
 
-function LightHelpers({ pointLightRef, directionalLightRef }) {
-  const { scene } = useThree();
+const InteractiveDisketa = ({ children, initialPosition, floatSpeed = 0.5, amplitude = 0.2, phaseOffset = 0 }) => {
+  const ref = useRef();
+  const [hovered, setHovered] = useState(false);
+  const isMobile = useIsMobile();
+  const originalRotation = new THREE.Vector3(0, 0, 0);
 
-  // Adding helpers to the scene when the component mounts
-  useMemo(() => {
-    if (pointLightRef.current) {
-      const pointLightHelper = new PointLightHelper(pointLightRef.current, 1);
-      scene.add(pointLightHelper);
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const { mouse, viewport } = state;
+
+    const mouseX = (mouse.x * viewport.width) / 2;
+    const mouseY = (mouse.y * viewport.height) / 2;
+
+    const rotationScaleX = 0.02;
+    const rotationScaleY = 0.02;
+
+    if (hovered && !isMobile) {
+      ref.current.rotation.x = THREE.MathUtils.lerp(ref.current.rotation.x, mouseY * rotationScaleX, 0.1);
+      ref.current.rotation.y = THREE.MathUtils.lerp(ref.current.rotation.y, mouseX * rotationScaleY, 0.1);
+    } else {
+      ref.current.rotation.x = THREE.MathUtils.lerp(ref.current.rotation.x, originalRotation.x, 0.1);
+      ref.current.rotation.y = THREE.MathUtils.lerp(ref.current.rotation.y, originalRotation.y, 0.1);
     }
-    if (directionalLightRef.current) {
-      const directionalLightHelper = new DirectionalLightHelper(directionalLightRef.current, 1);
-      scene.add(directionalLightHelper);
-    }
-  }, [scene, pointLightRef, directionalLightRef]);
 
-  return null;
-}
+    ref.current.position.y = initialPosition[1] + Math.sin(time * floatSpeed + phaseOffset) * amplitude;
+  });
+
+  return (
+    <group
+      ref={ref}
+      position={initialPosition}
+      onPointerOver={() => !isMobile && setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      {children}
+    </group>
+  );
+};
 
 export default function Carousel() {
   const [activeIndex, setActiveIndex] = useState(Math.floor(models.length / 2));
 
-  // Refs for lights to attach helpers
   const pointLightRef = useRef();
   const directionalLightRef = useRef();
 
-  // Swipeable handlers
   const handlers = useSwipeable({
     onSwipedLeft: () => handleNext(),
     onSwipedRight: () => handlePrev(),
@@ -53,28 +84,24 @@ export default function Carousel() {
     setActiveIndex((prevIndex) => (prevIndex - 1 + models.length) % models.length);
   };
 
-  // Calculate positions and scales for each item based on activeIndex
   const positions = useMemo(() => {
     return models.map((_, index) => {
       const offset = index - activeIndex;
       const scaleFactor = Math.max(0.5, 1 - Math.abs(offset) * 0.2);
-      const scale = [scaleFactor, scaleFactor, scaleFactor];
+      const scale = [scaleFactor * 2, scaleFactor * 0.9, scaleFactor * 0.9];
       const zPosition = -2 - Math.abs(offset);
-      const xPosition = offset * 4;
-      const position = offset === 0 ? [0, -5, -1] : [xPosition, -5, zPosition];
+      const xPosition = offset * 2.5;
+      const position = offset === 0 ? [0, -2.7, -1] : [xPosition, -2.7, zPosition];
       return { position, scale };
     });
   }, [activeIndex]);
 
   return (
-    <div
-      {...handlers}
-      className="flex justify-center items-center h-screen w-full bg-black"
-    >
+    <div {...handlers} className="flex justify-center items-center h-screen w-full bg-black pb-8">
       <div className="w-full h-full">
         <Canvas camera={{ position: [0, 0, 10], fov: 45 }}>
           <ambientLight intensity={0.5} />
-          <pointLight ref={pointLightRef} position={[5, -10, 10]} intensity={100} color={"#ff758f"}/>
+          <pointLight ref={pointLightRef} position={[5, -10, 10]} intensity={100} color={"#ff758f"} />
           <directionalLight ref={directionalLightRef} position={[5, -10, 13]} intensity={1} />
 
           {models.map((ModelComponent, index) => {
@@ -87,16 +114,27 @@ export default function Carousel() {
             });
 
             return (
-              <animated.group
+              <InteractiveDisketa
                 key={index}
-                position={animatedPosition.to((x, y, z) => [x, y, z])}
-                rotation={[0, -Math.PI / 2, 0]}
-                scale={animatedScale}
+                initialPosition={position}
+                floatSpeed={0.5}
+                amplitude={0.2}
+                phaseOffset={index}
               >
-                <ModelComponent />
-              </animated.group>
+                <animated.group
+                  position={animatedPosition.to((x, y, z) => [x, y, z])}
+                  rotation={[0, -Math.PI / 2, 0]}
+                  scale={animatedScale}
+                >
+                  <ModelComponent />
+                </animated.group>
+              </InteractiveDisketa>
             );
           })}
+
+          <InteractiveDisketa initialPosition={[-1, -1, 0]} floatSpeed={0.5} amplitude={0.2} phaseOffset={0}>
+            <TitluGD  rotation={[0, -Math.PI / 2, 0]} scale={[0.8, 0.8, 0.8]} />
+          </InteractiveDisketa>
         </Canvas>
       </div>
     </div>
